c6090ea68756c1b029c7d6fcc05a52b7c1bedbee
maksgirya <maksgirya@mail.ru>
2023-12-10 17:35:32 +0300

    hi there

---
+++ src/test/java/com/maksgir/sneakers/SneakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksgir.sneakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
